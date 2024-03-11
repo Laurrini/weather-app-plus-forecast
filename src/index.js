d6bec3c2c5ecf38723585c30dac8ed9012e71e3d
Laurrini <154939183+Laurrini@users.noreply.github.com>
2024-03-10 22:36:48 -0600

    Injected icon, temperatures and days form the API

--- src/index.js
+++ src/index.js
@@ -55,6 +55,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "aae4390da28do47e0e9dfc3fdaftcdbb";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -62,24 +69,27 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
         <div class="weather-forecast-date">
-        <div class="weather-forecast-day">${day}</div>
-        <div class="weather-forecast-icon">🌤</div>
+        <div class="weather-forecast-day">${formatDay(day.time)}</div>
+        <img/ src="${day.condition.icon_url}" class="weather-forecast-icon">
         <div class="weather-temperatures">
-        <span class="weather-temperature-max"> 19 º</span>
-        <span class="weather-temperature-min"> 7 º</span>
+        <span class="weather-temperature-max"> ${Math.round(
+          day.temperature.maximum
+        )} º</span>
+        <span class="weather-temperature-min"> ${Math.round(
+          day.temperature.minimum
+        )}  º</span>
         </div>
         </div>
         `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
